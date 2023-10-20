4dde4276945a7b341cec548c39508abee790c030
Gretchen Hilt <gretchen.hilt@selu.edu>
2023-10-20 20:32:46 +0000

    committing anova - no code yet.

---
+++ hilt.pkg/data/function_anova.R
@@ -0,0 +1,5 @@
+#code here
+
+
+The purpose of this function is to be able to put in any two categorical data columns and automatically create an anova out of it
+I will add a residual section so that I can compare the residuals as well. 
