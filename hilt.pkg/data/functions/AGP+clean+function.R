67994261d5c2f15025238fb9a3149bf67d5b405e
Gretchen Hilt <gretchen.hilt@selu.edu>
2023-10-20 19:13:31 +0000

    try 3?

--- hilt.pkg/data/functions/AGP+clean+function.R
+++
@@ -1,8 +0,0 @@
-remove_na_AGP <- function(data){
-  clean <- na.omit(data)
-  return(clean)
-}
-
-This function was created to clean up the data by removing all the NAs from the dataset. 
-I purposefully put a print component to the function so that when I run this function, it will confirm the absense of NAs by saying that there are no NAs found. 
-##DONE
--- hilt.pkg/data/functions/AGP+plotsvl+function.R
+++
@@ -1,8 +0,0 @@
-plotsvl_AGP <- function(data){
-  byspecies <- ggplot(data = AGP, mapping = aes(x = critter, y = svl, group = critter)) + geom_boxplot()
-  return(byspecies)
-}
-plotsvl_AGP(AGP)
-
-This function was designed to plot the critters and their svls relative to each other in a boxplot.
-##DONE
