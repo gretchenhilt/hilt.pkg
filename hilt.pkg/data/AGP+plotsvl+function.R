06a6d3c86cface4a04240a3ed4b857f247b9cc1d
Gretchen Hilt <gretchen.hilt@selu.edu>
2023-10-20 18:42:56 +0000

    Pushing first packages to github.

---
+++ hilt.pkg/data/AGP+plotsvl+function.R
@@ -0,0 +1,8 @@
+plotsvl_AGP <- function(data){
+  byspecies <- ggplot(data = AGP, mapping = aes(x = critter, y = svl, group = critter)) + geom_boxplot()
+  return(byspecies)
+}
+plotsvl_AGP(AGP)
+
+This function was designed to plot the critters and their svls relative to each other in a boxplot.
+##DONE
